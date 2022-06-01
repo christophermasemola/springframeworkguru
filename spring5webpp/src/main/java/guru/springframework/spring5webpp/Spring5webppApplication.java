59f576afe5e4b7e70a11ffa0639ff9b20f60b7ac
thabang.charlie <thabang.charlie@pilog.co.za>
2022-06-01 05:37:37 +0200

    create SpringFrameworkGuru project

--- spring5webpp/src/main/java/guru/springframework/spring5webpp/Spring5webppApplication.java
+++ spring5webpp/src/main/java/guru/springframework/spring5webpp/Spring5webppApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Spring5webppApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Spring5webppApplication.class, args);
+		//SpringApplication.run(Spring5webppApplication.class, args);
 	}
 
 }
